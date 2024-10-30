ab9abf3dff884e7917365e473ada3577688c8a6a
Porsche <worrapon12345@gmail.com>
2024-10-31 02:31:34 +0700

    แก้สีและresponse

--- menu.js
+++
@@ -1,8 +0,0 @@
-const hamMenu = document.querySelector(".navbar-toggler");
-const offScreenMenu = document.querySelector(".navbar-collapse");
-
-hamMenu.addEventListener("click", () => {
-    hamMenu.classList.toggle("active");
-    offScreenMenu.classList.toggle("active");
-});
-
